66241bd1fc73223f16582d84745993722f734d67
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-01-12 17:05:24 -0700

    Built comp3705 from commit bd9753f on branch master

---
+++ server/config/environment/test.js
@@ -0,0 +1,13 @@
+'use strict';
+/*eslint no-process-env:0*/
+
+// Test specific configuration
+// ===========================
+
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/comp3705-test'
+  }
+};
+//# sourceMappingURL=test.js.map
