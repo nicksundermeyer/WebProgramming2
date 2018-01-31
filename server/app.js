13733acb04f019af4ffd5a07d72a822175cb65f5
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-01-30 17:03:36 -0700

    Completed COMP-3705 lab 08

--- server/app.js
+++ server/app.js
@@ -5,8 +5,17 @@
 'use strict';
 
 import express from 'express';
+import mongoose from 'mongoose';
+mongoose.Promise = require('bluebird');
 import config from './config/environment';
 import http from 'http';
+
+// Connect to MongoDB
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function (err) {
+  console.error(`MongoDB connection error: ${err}`);
+  process.exit(-1); // eslint-disable-line no-process-exit
+});
 
 // Setup server
 var app = express();
@@ -16,7 +25,7 @@ require('./routes').default(app);
 
 // Start server
 function startServer() {
-  app.comp3705 = server.listen(config.port, config.ip, function() {
+  app.comp3705 = server.listen(config.port, config.ip, function () {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   });
 }
