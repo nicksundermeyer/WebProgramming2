32ff5b7033d36d71af8f4ac80e1556fc4aca94c6
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-02-01 17:36:03 -0700

    Completed COMP-3705 lab 09

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import * as controller from './users.controller';
 
-// create new router
 let router = express.Router();
 
-// export the router
-export { router };
-
-// register GET routes for /users API
+// GET methods
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 
-// register POST routes for /users API
+// POST method
 router.post('/', controller.create);
 
-// register PUT and DELETE routes for /users API
-router.put('/:id', controller.upsert);
+// PUT method
+router.put('/:id', controller.update);
+
+// DELETE method
 router.delete('/:id', controller.destroy);
+
+export { router };
